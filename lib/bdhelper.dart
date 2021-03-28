0f85ecacf8153b4721cb7a75c597e1c310d43113
your.user.name <38698778+Rakibul25@users.noreply.github.com>
2021-03-29 02:10:06 +0600

    initialing db done

--- lib/bdhelper.dart
+++ lib/bdhelper.dart
@@ -1,4 +1,26 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DataBaseHelper {
+  static final _dbname = 'mydb.db';
+  static final _dbversion = 1;
+
   DataBaseHelper._privateConstructor();
   static final DataBaseHelper instance = DataBaseHelper._privateConstructor();
+
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initialDatabase();
+    return database;
+  }
+
+  _initialDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, _dbname);
+    await openDatabase(path, version: _dbversion, onCreate: _oncreate;
+  }
 }
