094d4409e1a8f64624adde63713c5ded84704f3b
your.user.name <38698778+Rakibul25@users.noreply.github.com>
2021-04-04 00:11:27 +0600

    all four button addition done in front end

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,21 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('test sqf'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+                onPressed: () {}, child: Text('Insert'), color: Colors.red),
+            FlatButton(
+                onPressed: () {}, child: Text('query'), color: Colors.blue),
+            FlatButton(
+                onPressed: () {}, child: Text('Update'), color: Colors.green),
+            FlatButton(
+                onPressed: () {}, child: Text('Delete'), color: Colors.grey),
+          ],
+        ),
+      ),
     );
   }
 }
